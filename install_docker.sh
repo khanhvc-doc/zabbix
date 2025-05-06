2e4e009e33f29445cda4f396d6bd4a229ff06167
KhanhVC <khanhvc.doc@gmail.com>
2025-05-06 10:03:38 +0700

    1 install docker, 2 install zabbiz v2

--- install_docker.sh
+++ install_docker.sh
@@ -26,6 +26,7 @@ echo "Installing Docker Compose..."
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+echo "Docker installation completed successfully!"
 
 # Add current user to docker group
 echo "Adding user to docker group..."
